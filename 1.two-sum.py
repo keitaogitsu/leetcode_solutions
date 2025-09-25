f4815419974f6ad52d8297e94e83b843a84127a3
keitaogitsu <ogitsukeita@gmail.com>
2025-09-26 08:41:52 +0900

    1 Listをlistに変更

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -6,7 +6,7 @@
 
 # @lc code=start
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
