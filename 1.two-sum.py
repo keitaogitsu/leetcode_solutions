5c27c8b9a54489412f072fe6144f626022dcb336
keitaogitsu <ogitsukeita@gmail.com>
2025-09-26 08:34:52 +0900

    1 completed

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -7,5 +7,9 @@
 # @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
 # @lc code=end
 
