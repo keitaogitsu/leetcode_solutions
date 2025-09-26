727d1c73ba11995e3d3b5bcb3b574d84005907de
keitaogitsu <ogitsukeita@gmail.com>
2025-09-26 09:30:14 +0900

    1 hashテーブルの使用

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -7,9 +7,14 @@
 # @lc code=start
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        numMap = {}
+        n = len(nums)
+
+        for i in range(n):
+            complement = target - nums[i]
+            if complement in numMap:
+                return [numMap[complement], i]
+            numMap[nums[i]] = i
+        return []
 # @lc code=end
 
